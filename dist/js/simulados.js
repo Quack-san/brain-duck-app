85b5cbc8a917212096ed9e46084fba133db3f951
Helton Rodrigues <heltoncr.office@gmail.com>
2023-02-16 22:38:32 -0300

    v 0.1 Feat: added simulados.js. This file makes the job to show and hide the test divs and call the testSimulate.html

--- dist/js/simulados.js
+++ dist/js/simulados.js
@@ -1,56 +1,71 @@
 const instalacoesArray = [
-    {"name": "ETEC", "year": 2020},
-    {"name": "ETEC", "year": 2021},
-    {"name": "ETEC", "year": 2022}
+    {"name": "ETEC", "year": 2020, "id": "abc"},
+    {"name": "ENEM", "year": 2012, "id": "abc"},
+    {"name": "ETEC", "year": 2021, "id": "abc"},
+    {"name": "ETEC", "year": 2022, "id": "abc"},
+    {"name": "ENEM", "year": 2019, "id": "abc"},
+    {"name": "FUVEST", "year": 1992, "id": "abc"},
+    {"name": "ENEM", "year": 2061, "id": "abc"},
+    {"name": "FUVEST", "year": 1990, "id": "abc"},
+    {"name": "HAVARD", "year": 2000, "id": "abc"}
 ] 
 
 var institutionsDivsAlreadyExists = []
 
-instalacoesArray.forEach( (test ) => {
+instalacoesArray.forEach( (test) => {
+    console.log("array function initialized");
     var divRoot = document.getElementById("examContainer");
     if (!checkExistInstitution(test.name)) {
+        console.log("inside if condition");
         // This five first lines creates a container with the name of the institution inside.
         var newLabelContainer = document.createElement("div");
+        newLabelContainer.classList.add(
+            "testBox", 
+            "bg-info", 
+            "m-2", 
+            "text-center",
+            "rounded"
+            );
+        setAttributes(newLabelContainer, {
+            "role": "button"
+        })
+        console.log("after set attributes function");
+
         var newLabelTest = document.createElement("span");
-        newLabelContainer.appendChild(newLabelTest);
+        newLabelTest.classList.add("testLabel");
+
         var labelTestText = document.createTextNode(test.name);
         newLabelTest.appendChild(labelTestText);
+        newLabelContainer.appendChild(newLabelTest);
+
 
         newLabelContainer.addEventListener("click", showHideTests);
-    }
-})
-
-instalacoesArray.forEach( (test) => {
-    var divRoot = document.getElementById("examContainer");
-    if (!checkExistInstitution(test.name)) {
-        var newLabelTest = document.createElement("button");
-        var labelTestText = document.createTextNode(test.name);
-        newLabelTest.appendChild(labelTestText);
-        newLabelTest.addEventListener("click", showHideTests);
-        divRoot.appendChild(newLabelTest);       
+        divRoot.appendChild(newLabelContainer);
 
         var newDivTest = document.createElement("div");
         newDivTest.setAttribute("id", test.name);
         divRoot.appendChild(newDivTest);
 
         institutionsDivsAlreadyExists.push(test.name);
-    } 
+    }
 
     var newTest = document.createElement("button");
     var newTestText = document.createTextNode(test.year);
     newTest.appendChild(newTestText);
     newTest.classList.add(test.name, "test");
-    newTest.setAttribute("test", test.name)
+    newTest.setAttribute("test", test.name);
+    newTest.setAttribute("id", test.id)
     newTest.style.display = "none";
-    divRoot.appendChild(newTest);
+    newTest.addEventListener('click', selectTest);
+    document.getElementById(test.name).appendChild(newTest);
 })
 
 function checkExistInstitution(institution) {
+    console.log("check function executed");
     let stateReturn = false;
     
     institutionsDivsAlreadyExists.forEach((div) => {
         if (institution == div) { 
-            console.log("return true")
             stateReturn = true; 
         } 
     })
@@ -58,6 +73,7 @@ function checkExistInstitution(institution) {
 }
 
 function showHideTests() {
+    console.log("colapse function executed");
     var testClicked = this.innerText;
     document.querySelectorAll(".test").forEach((test) => {
          var testName = test.getAttribute("test");
@@ -70,4 +86,14 @@ function showHideTests() {
             }
         }
     })
+}
+
+function setAttributes(el, attrs) {
+    for(var index in attrs) {
+      el.setAttribute(index, attrs[index]);
+    }
+}
+
+function selectTest() {
+    window.location.href = ("testSimulate.html?idtest="+this.getAttribute("id"));
 }
---
+++ dist/js/testSimulate.js
@@ -0,0 +1,2 @@
+const urlParams = new URLSearchParams(window.location.search);
+const idtest = urlParams.get('idtest');
