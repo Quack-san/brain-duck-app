8733e79d9316eee41f0caafab492e86a66d6589b
Helton Rodrigues <heltoncr.office@gmail.com>
2023-02-18 17:56:16 -0300

    Style and Feat: Stylized better the buttons of each test element, Added a function that hide any unfocused instituition

--- dist/js/simulados.js
+++ dist/js/simulados.js
@@ -1,74 +1,84 @@
 const instalacoesArray = [
-    {"name": "ETEC", "year": 2020, "id": "abc"},
-    {"name": "ENEM", "year": 2012, "id": "abc"},
-    {"name": "ETEC", "year": 2021, "id": "abc"},
-    {"name": "ETEC", "year": 2022, "id": "abc"},
-    {"name": "ENEM", "year": 2019, "id": "abc"},
-    {"name": "FUVEST", "year": 1992, "id": "abc"},
-    {"name": "ENEM", "year": 2061, "id": "abc"},
-    {"name": "FUVEST", "year": 1990, "id": "abc"},
-    {"name": "HARVARD", "year": 2000, "id": "abc"}
+    {"name": "ETEC", "year": 2020, "id": "etec2020"},
+    {"name": "ENEM", "year": 2012, "id": "enem2012"},
+    {"name": "ETEC", "year": 2021, "id": "etec2021"},
+    {"name": "ETEC", "year": 2022, "id": "etec2022"},
+    {"name": "ENEM", "year": 2019, "id": "enem2019"},
+    {"name": "FUVEST", "year": 1992, "id": "fuvest1992"},
+    {"name": "ENEM", "year": 2061, "id": "enem2061"},
+    {"name": "FUVEST", "year": 1990, "id": "fuvest1990"},
+    {"name": "HARVARD", "year": 2000, "id": "harvard2000"}
 ] 
 
 var institutionsDivsAlreadyExists = []
 
 instalacoesArray.forEach( (test) => {
-    console.log("array function initialized");
+    console.log("Operation Realized: ");
     var divRoot = document.getElementById("examContainer");
     if (!checkExistInstitution(test.name)) {
-        console.log("inside if condition");
         // This five first lines creates a container with the name of the institution inside.
-        var newLabelContainer = document.createElement("div");
-        newLabelContainer.classList.add(
+        var btnInstitutionContainer = document.createElement("button");
+        btnInstitutionContainer.classList.add(
             "testBox",
-            "m-2", 
             "text-center",
             "rounded",
-            "border",
-            "shadow"
+            "btn",
+            "shadow",
             );
-        setAttributes(newLabelContainer, {
-            "role": "button",
-            "style": "background-color: #ff9900",
+        setAttributes(btnInstitutionContainer, {
+            "style": "background-color: #ff9900"
             })
-        console.log("after set attributes function");
 
-        var newLabelTest = document.createElement("span");
-        newLabelTest.classList.add(
+        var institutionLabel = document.createElement("span");
+        institutionLabel.classList.add(
             "testLabel"
             );
 
         var labelTestText = document.createTextNode(test.name);
-        newLabelTest.appendChild(labelTestText);
-        newLabelContainer.appendChild(newLabelTest);
+        institutionLabel.appendChild(labelTestText);
+        btnInstitutionContainer.appendChild(institutionLabel);
 
 
-        newLabelContainer.addEventListener("click", showHideTests);
-        divRoot.appendChild(newLabelContainer);
+        btnInstitutionContainer.addEventListener("click", showHideTests);
+        divRoot.appendChild(btnInstitutionContainer);
 
-        var newDivTest = document.createElement("div");
-        newDivTest.setAttribute("id", test.name);
-        divRoot.appendChild(newDivTest);
+        var divboxAllTests = document.createElement("div");
+        divboxAllTests.classList.add(
+            "testListDiv",
+            "row"
+        )
+        divboxAllTests.setAttribute("id", test.name);
+        divboxAllTests.style.display = "none";
+        divRoot.appendChild(divboxAllTests);
 
         institutionsDivsAlreadyExists.push(test.name);
     }
 
-    var newTest = document.createElement("button");
-    var newTestText = document.createTextNode(test.year);
-    newTest.appendChild(newTestText);
-    newTest.classList.add(
+    var singleTest = document.createElement("button");
+    var singleTestLabel = document.createElement("span");
+    var singleTestText = document.createTextNode(test.year);
+
+    singleTestLabel.appendChild(singleTestText);
+    singleTestLabel.classList.add(
+        "border-bottom",
+        "border-secondary",
+    )
+    singleTest.appendChild(singleTestLabel);
+    singleTest.classList.add(
         test.name,
-        "test"
+        "test",
+        "btn"
         );
-    newTest.setAttribute("test", test.name);
-    newTest.setAttribute("id", test.id)
-    newTest.style.display = "none";
-    newTest.addEventListener('click', selectTest);
-    document.getElementById(test.name).appendChild(newTest);
+    setAttributes(singleTest, {
+        "test": test.name,
+        "id": test.id,
+        "style": "font-size: large"
+        });
+    singleTest.addEventListener('click', selectTest);
+    document.getElementById(test.name).appendChild(singleTest);
 })
 
 function checkExistInstitution(institution) {
-    console.log("check function executed");
     let stateReturn = false;
     
     institutionsDivsAlreadyExists.forEach((div) => {
@@ -80,19 +90,21 @@ function checkExistInstitution(institution) {
 }
 
 function showHideTests() {
-    console.log("colapse function executed");
-    var testClicked = this.innerText;
-    document.querySelectorAll(".test").forEach((test) => {
-         var testName = test.getAttribute("test");
+    var divWithAllTest = document.getElementById(this.innerText);
 
-        if (testName == testClicked) {
-            if (test.style.display == "none") {
-            test.style.display = "block";
-            } else {
-                test.style.display = "none";
-            }
+        if (divWithAllTest.style.display == "none") {
+            divWithAllTest.style.display = "block";
+        } else {
+            divWithAllTest.style.display = "none";
         }
-    })
+    
+    document.querySelectorAll("div.testListDiv").forEach( (div) => {
+        var divID = div.getAttribute("id");
+        console.log(divID);
+        if (this.innerText != divID) {
+            div.style.display = "none";
+        }
+    });
 }
 
 function setAttributes(elem, attrs) {
