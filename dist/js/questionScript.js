eef0b37f37588db0117aedd0e9972b20c062cb6d
Helton Rodrigues <heltoncr.office@gmail.com>
2023-03-03 19:25:39 -0300

    Added: Script of questions.html

---
+++ dist/js/questionScript.js
@@ -0,0 +1,35 @@
+var institutions = ["ETEC", "ENEM"];
+var subjects = ["Ingreis", "Math"];
+
+document.querySelectorAll(".search-item").forEach( (searchItem) => {
+    searchItem.addEventListener('click', (event) => {
+        var targetID = event.target.getAttribute("id");
+        
+        if (targetID == "searchInstitution") { requestItems(institutions) }
+        else if (targetID == "searchSubject") { requestItems(subjects) }
+        else if (targetID == "searchSubSubject") { clearList() }
+    })
+})
+
+function requestItems (itemsArray) {
+    clearList()
+
+    itemsArray.forEach( (item) => {
+        document.querySelector('.search-grid').appendChild(
+            Object.assign(
+                document.createElement('div'),
+                { className : "subject-item" },
+                { role : "button" },
+                { innerHTML : item }
+            )
+        )
+    })
+}
+
+function clearList() {
+    var root = document.querySelector(".search-grid");
+    var toDelete = document.querySelectorAll(".subject-item");
+    toDelete.forEach((el) => {
+        el.remove();
+    })
+}
