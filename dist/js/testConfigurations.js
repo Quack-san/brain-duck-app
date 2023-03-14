53056fcb7c612d753578de19be7ba56727fe914d
Helton Rodrigues <heltoncr.office@gmail.com>
2023-03-13 22:20:30 -0300

    Style: created a time handler on testSimulate.html

--- dist/js/testConfigurations.js
+++ dist/js/testConfigurations.js
@@ -10,17 +10,38 @@ document.getElementById("btnStartTest").addEventListener('click', () => {
         return;
     }
     actualTest.style.display = "grid";
+
+    document.querySelector('.test-cover').style.display = 'none';
 });
 
-document.getElementById("showCustomTestTime").addEventListener('click', () => {
-    var spanHour = document.querySelector("#spanTimeHour");
-    var spanMinute = document.querySelector("#spanTimeMinute");
+document.getElementById('testTime').addEventListener('click', () => { timeDisplayHandler('show') })
+document.getElementById('cancelTime').addEventListener('click', () => { timeDisplayHandler('none') })
 
-    if (spanHour.style.display == "block") {
-        spanHour.style.display = "none";
-        spanMinute.style.display = "none";
-        return;
+function timeDisplayHandler(display) {
+    var timeBoxHadler = document.querySelector(".test-time-handler");
+    var veil = document.querySelector(".dark-veil");
+
+    switch (display) {
+        case 'show':
+            timeBoxHadler.style.display = "flex";
+            veil.style.display = "block";
+            break;
+        case 'none':
+            timeBoxHadler.style.display = "none";
+            veil.style.display = "none";
+            break;
     }
-    spanHour.style.display = "block";
-    spanMinute.style.display = "block";
-});
+}
+
+// document.getElementById("showCustomTestTime").addEventListener('click', () => {
+//     var spanHour = document.querySelector("#spanTimeHour");
+//     var spanMinute = document.querySelector("#spanTimeMinute");
+
+//     if (spanHour.style.display == "block") {
+//         spanHour.style.display = "none";
+//         spanMinute.style.display = "none";
+//         return;
+//     }
+//     spanHour.style.display = "block";
+//     spanMinute.style.display = "block";
+// });
