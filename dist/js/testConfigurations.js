9567bded3f81f323187de41eb90663d8c0ceabc7
Helton Rodrigues <heltoncr.office@gmail.com>
2023-03-17 20:25:22 -0300

    Style: testSimulate.html refatored css layout

--- dist/js/testConfigurations.js
+++ dist/js/testConfigurations.js
@@ -3,6 +3,18 @@
     JavaScript document used to configure the params of the test
 */
 
+var errorAlert = document.querySelector(".error-alert");
+var inputHour = document.getElementById('hour');
+var inputMinute = document.getElementById('minute');
+
+document.getElementById('testTime').addEventListener('click', () => { timeDisplayHandler('show') })
+document.getElementById('cancelTime').addEventListener('click', () => { timeDisplayHandler('none') })
+document.getElementById('saveTime').addEventListener('click', () => { saveTime(timer) } )
+
+var timer;
+var hour;
+var minute;
+
 document.getElementById("btnStartTest").addEventListener('click', () => {
     var actualTest = document.querySelector('.test-ground')
     if (actualTest.style.display == "grid") {
@@ -14,23 +26,13 @@ document.getElementById("btnStartTest").addEventListener('click', () => {
     document.querySelector('.test-cover').style.display = 'none';
 });
 
-document.getElementById('testTime').addEventListener('click', () => { timeDisplayHandler('show') })
-document.getElementById('cancelTime').addEventListener('click', () => { timeDisplayHandler('none') })
 
 function timeDisplayHandler(display) {
     var timeBoxHadler = document.querySelector(".test-time-handler");
-    var veil = document.querySelector(".dark-veil");
 
-    switch (display) {
-        case 'show':
-            timeBoxHadler.style.display = "flex";
-            veil.style.display = "block";
-            break;
-        case 'none':
-            timeBoxHadler.style.display = "none";
-            veil.style.display = "none";
-            break;
-    }
+    if (display == "show") { timeBoxHadler.style.display = "flex"; veilReveal("block") }
+    else { timeBoxHadler.style.display = "none"; veilReveal("none") }
+
 }
 
 // Code to manually select the timer
@@ -40,9 +42,6 @@ document.querySelectorAll(".handler").forEach( (button) => {
 })
 
 function timerChanger(btn) {
-    var inputHour = document.getElementById('hour');
-    var inputMinute = document.getElementById('minute');
-
     var timerNow = parseInt(inputHour.value) * 60;
     timerNow += parseInt(inputMinute.value);
 
@@ -50,12 +49,26 @@ function timerChanger(btn) {
     if (btn.classList.contains('hour')) { modifier = 60 } else { modifier = 10 }
     if (btn.classList.contains('increase')) { timerNow += modifier } else { timerNow -= modifier}
 
-    if (timerNow > 720) { timerNow = 720 } else if ( timerNow < 0) { timerNow = 0 }
+    if (timerNow > 720) { timerNow = 720 } else if ( timerNow < 1) { timerNow = 0 }
 
-    var hour = Math.floor(timerNow / 60);
+    timer = timerNow;
+
+    hour = Math.floor(timerNow / 60);
     timerNow = timerNow % 60;
-    var minute = timerNow;
+    minute = timerNow;
 
     inputHour.value = hour < 10 ? "0"+hour : hour;
     inputMinute.value = minute <= 0 ? "00" : minute;
 }
+
+// Sanitaze user selected timer
+
+function saveTime(timer) { timer <= 0 ? errorMenssage('O TEMPO SELECIONADO É INVÁLIDO') : timeDisplayHandler("none"); console.log(timer); }
+
+function veilReveal(visibility) { document.querySelector(".dark-veil").style.display = visibility }
+
+function errorMenssage(text) {
+    errorAlert.style.display = "flex";
+    errorAlert.childNodes.item(1).innerText = text;
+}
+document.querySelector(".alert-ok").addEventListener('click', () => { errorAlert.style.display = "none" } )
