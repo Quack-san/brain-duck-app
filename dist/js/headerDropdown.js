2543d6d1da5666ab9f89f47b7690511f70f1fa7f
Helton Rodrigues <heltoncr.office@gmail.com>
2023-05-03 18:09:45 -0300

    Chenges on work

--- dist/js/headerDropdown.js
+++ dist/js/headerDropdown.js
@@ -1,6 +1,6 @@
-dropdown = document.querySelector(".dropdown");
+dropdown = document.querySelector(".link-list");
 document.querySelector(".toggle").addEventListener('click', () => {
-    dropdown.style.display = dropdown.style.display == "none" ? "grid" : "none";
+    dropdown.style.display = dropdown.style.display == "none" ? "block" : "none";
 })
 addEventListener('resize', () => {
     dropdown.style.display = "none"
